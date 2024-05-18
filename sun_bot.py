ed6dd4b1c954249f02792f4f567485c4b4737423
YooSunyoung <luysunyoung9@gmail.com>
2024-05-18 15:44:02 +0200

    improved

--- sun_bot.py
+++ sun_bot.py
@@ -9,6 +9,32 @@ from cholerama import Positions, helpers
 AUTHOR = "SourDough"  # This is your team name
 SEED = None  # Set this to a value to make runs reproducible
 
+def _rotate_90(pattern: Positions) -> Positions:
+    return Positions(
+        x=pattern.y[::-1], y=pattern.x[::-1]
+    )
+
+
+def rotate_pattern(pattern: Positions, n_90: int) -> Positions:
+    """
+    Rotate the pattern 90 degrees n_90 times.
+    """
+    if n_90%4 > 0:
+        return rotate_pattern(_rotate_90(pattern), n_90-1)
+    return _rotate_90((pattern))
+
+
+_FENTOMIO = Positions(  # fentomino pattern 
+    x = np.array([1, 0, 1, 1, 2]),
+    y = np.array([0, 1, 1, 2, 2])
+)
+
+def place_pattern(pattern: Positions, empty_space: np.ndarray) -> Positions:
+    return Positions(
+        x = pattern.x + empty_space[1],
+        y = pattern.y + empty_space[0],
+    )
+
 
 class Bot:
     """
@@ -44,14 +70,10 @@ class Bot:
         self.patch_size = patch_size
 
         self.rng = np.random.default_rng(SEED)
-
-        # If we make the pattern too sparse, it just dies quickly
-        # xy = self.rng.integers(0, 12, size=(2, 100))
-        # self.pattern = Positions(
-        #     x=xy[1] + patch_size[1] // 2, y=xy[0] + patch_size[0] // 2
-        # )
-        # The pattern can also be just an image (0=white, 1=black)
         self.pattern = "start_pattern.png"
+
+    def count_my_cells(self, board: np.ndarray) -> int:
+        return sum(sum(board == self.number))
 
     def iterate(
         self, iteration: int, board: np.ndarray, patch: np.ndarray, tokens: int
@@ -74,17 +96,17 @@ class Bot:
         -------
         An object containing the x and y coordinates of the new cells.
         """
-        if tokens >= 10:
-            # Pick a random empty region of size 3x3 inside my patch
-            empty_regions = helpers.find_empty_regions(patch, (3, 3))
-            nregions = len(empty_regions)
-            if nregions == 0:
-                return None
-            # Make a glider
-            ind = self.rng.integers(0, nregions)
-            # x = np.array([1, 2, 0, 1, 2]) + empty_regions[ind, 1]
-            # y = np.array([2, 1, 0, 0, 0]) + empty_regions[ind, 0]
-            return Positions(  # fentomino pattern 
-                x = np.array([1, 0, 1, 1, 2])+ empty_regions[ind, 1],
-                y = np.array([0, 1, 1, 2, 2]) + empty_regions[ind, 0],
+        empty_regions = helpers.find_empty_regions(patch, (3, 3))
+        nregions = len(empty_regions)
+        ind = self.rng.integers(0, nregions)
+        if nregions == 0:
+            return None
+
+        if tokens >= 5 and (self.count_my_cells(board) < 200): # I'm dying!
+            return place_pattern(
+                rotate_pattern(_FENTOMIO, self.rng.integers(0, 4)), empty_regions[ind]
+            )
+        elif tokens >= 5 and (self.rng.integers(0, 10) <= int(iteration/400)):
+            return place_pattern(
+                rotate_pattern(_FENTOMIO, self.rng.integers(0, 4)), empty_regions[ind]
             )
